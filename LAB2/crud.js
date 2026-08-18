c8b2638b15ac891823ed08b0a3d7567f47b6f795
Ajay Kumar Mishra <ajajymishra32@gmail.com>
2026-08-18 14:50:55 +0530

    crud.js

--- LAB2/crud.js
+++ LAB2/crud.js
@@ -1,5 +1,25 @@
 import readline  from "readline/promises";
+import {writeFile , readFile}from"fs/promises";
 import{stdin,stdout } from "process";
+ 
+const FILE ="products.json"
+
+const saveCart=async(cart)=>{
+    await writeFile(FILE,JSON.stringify(cart,null,2));
+};
+const getCart=async()=>{
+    const data = await readFile(FILE,"utf-8");
+    return JSON.parse(data);
+};
+
+const addTocart=async(item)=>{
+      const products= await getCart();
+      products.push(item);
+      await saveCart(products);
+};
+
+
+
 const main = async()=>{
      const cin = readline.createInterface({input:stdin,output: stdout });
      let choice;
