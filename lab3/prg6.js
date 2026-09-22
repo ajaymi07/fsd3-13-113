c31aeca54d85f308b9471153a674ad6df4d45710
Ajay Kumar Mishra <ajajymishra32@gmail.com>
2026-09-22 14:05:37 +0530

    prg6

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,9 +1,17 @@
 import http from "http";
+import  {getAllProducts} from "./products.js";
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
+  if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
-    res.end("GET Request");
+    const data =getAllProducts();
+    res.setHeader('content-type','application/json')
+    res.end(
+      JSON.stringify({
+       count:data.length,
+       data,
+    }),
+    );
   }
   else if (req.url === "/" && req.method === "POST") {
     let body = "";
---
+++ lab3/products.js
@@ -0,0 +1,10 @@
+const products=[
+    {id:1,name :'maker',qty:100,price:115},
+    {id:2,name:'duster',qty:50,price:10},
+]
+
+let nextID=3;
+
+ export const getAllProducts = () =>{
+    return products; 
+}
